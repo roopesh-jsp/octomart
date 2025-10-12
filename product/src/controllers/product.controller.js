5f51876ffe819ce559d0c8e56cbd3b886a3fbb44
roopesh-jsp <rupzkumar5@gmail.com>
2025-10-12 20:59:05 +0530

    added del and update apis for products

--- product/src/controllers/product.controller.js
+++ product/src/controllers/product.controller.js
@@ -1,5 +1,6 @@
 const { uploadImage } = require("../services/imagekit.service");
 const productModel = require("../models/product.model");
+const { default: mongoose } = require("mongoose");
 
 async function addProduct(req, res) {
   try {
@@ -73,6 +74,9 @@ async function getProducts(req, res) {
 async function getProductById(req, res) {
   try {
     const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "invalid id" });
+    }
     const product = await productModel.findById(id);
     if (!product) {
       return res.status(404).json({ message: "product not found" });
@@ -83,8 +87,93 @@ async function getProductById(req, res) {
     return res.status(500).json({ message: "internal server error" });
   }
 }
+
+async function updateProducts(req, res) {
+  try {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "invalid id" });
+    }
+    const product = await productModel.findOne({
+      _id: id,
+    });
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    if (product.seller.toString() !== req.user.id) {
+      return res
+        .status(403)
+        .json({ message: "Forbidden: You can only update your own products" });
+    }
+
+    const allowedUpdates = ["title", "description", "price"];
+    for (const key of Object.keys(req.body)) {
+      if (allowedUpdates.includes(key)) {
+        if (key === "price" && typeof req.body.price === "object") {
+          if (req.body.price.amount !== undefined) {
+            product.price.amount = Number(req.body.price.amount);
+          }
+          if (req.body.price.currency !== undefined) {
+            product.price.currency = req.body.price.currency;
+          }
+        } else {
+          product[key] = req.body[key];
+        }
+      }
+    }
+    await product.save();
+    return res.status(200).json({ message: "Product updated", product });
+  } catch (error) {
+    console.log("error updating product data", error);
+    return res.status(500).json({ message: "internal server error" });
+  }
+}
+
+async function deleteProduct(req, res) {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ message: "Invalid product id" });
+  }
+
+  const product = await productModel.findOne({
+    _id: id,
+  });
+
+  if (!product) {
+    return res.status(404).json({ message: "Product not found" });
+  }
+
+  if (product.seller.toString() !== req.user.id) {
+    return res
+      .status(403)
+      .json({ message: "Forbidden: You can only delete your own products" });
+  }
+
+  await productModel.findOneAndDelete({ _id: id });
+  return res.status(200).json({ message: "Product deleted" });
+}
+
+async function getProductsBySeller(req, res) {
+  const seller = req.user;
+
+  const { skip = 0, limit = 20 } = req.query;
+
+  const products = await productModel
+    .find({ seller: seller.id })
+    .skip(skip)
+    .limit(Math.min(limit, 20));
+
+  return res.status(200).json({ data: products });
+}
+
 module.exports = {
   addProduct,
   getProducts,
   getProductById,
+  updateProducts,
+  deleteProduct,
+  getProductsBySeller,
 };
--- product/src/routes/product.route.js
+++ product/src/routes/product.route.js
@@ -19,10 +19,30 @@ porductRouter.post(
   productController.addProduct
 );
 
+//GET get all products of the seller
+porductRouter.get(
+  "/seller",
+  createAuthMiddleware(["seller"]),
+  productController.getProductsBySeller
+);
+
 //GET api/products/
 porductRouter.get("/", productController.getProducts);
 
 //GET api/products/:id
 porductRouter.get("/:id", productController.getProductById);
+
+//PATCH api/products/:id
+
+porductRouter.patch(
+  "/:id",
+  createAuthMiddleware(["seller"]),
+  productController.updateProducts
+);
+porductRouter.delete(
+  "/:id",
+  createAuthMiddleware(["seller"]),
+  productController.deleteProduct
+);
 
 module.exports = porductRouter;
